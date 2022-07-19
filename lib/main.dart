86e8ee78b2e363e246cef84ae03a56bda49fea0d
阿腾木 <cnatom@foxmail.com>
2022-07-19 20:46:21 +0800

    🎈 perf(main.dart): 延后了测试期限

--- lib/main.dart
+++ lib/main.dart
@@ -120,7 +120,7 @@ class _StartPageState extends State<StartPage> {
     //初始化配置
     initSize();
     //内测结束跳转
-    if(DateTime.now().isAfter(DateTime(2022,7,20))){
+    if(DateTime.now().isAfter(DateTime(2022,8,15))){
       toNullPage(context);
       return;
     }
