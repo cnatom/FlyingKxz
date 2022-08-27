0a4ea0caa2aa45ab53de9ea26f0ce0e4bc55e3cd
CUMT-Atom <cnatom@foxmail.com>
2022-08-27 20:38:47 +0800

    ✨feature(power):电量充值
    - 通过跳转的方式实现

--- lib/pages/navigator_page_child/myself_page_child/power/power_page.dart
+++ lib/pages/navigator_page_child/myself_page_child/power/power_page.dart
@@ -5,9 +5,11 @@ import 'package:flying_kxz/flying_ui_kit/Text/text.dart';
 import 'package:flying_kxz/flying_ui_kit/Theme/theme.dart';
 import 'package:flying_kxz/flying_ui_kit/appbar.dart';
 import 'package:flying_kxz/flying_ui_kit/config.dart';
+import 'package:flying_kxz/flying_ui_kit/dialog.dart';
 import 'package:flying_kxz/flying_ui_kit/loading.dart';
 import 'package:flying_kxz/flying_ui_kit/picker.dart';
 import 'package:flying_kxz/Model/prefs.dart';
+import 'package:flying_kxz/flying_ui_kit/toast.dart';
 import 'package:flying_kxz/pages/navigator_page.dart';
 import 'package:flying_kxz/pages/navigator_page_child/myself_page_child/power/utils/provider.dart';
 import 'package:material_design_icons_flutter/material_design_icons_flutter.dart';
@@ -39,9 +41,32 @@ class _PowerPageState extends State<PowerPage> {
   double powerPercent;
   bool powerLoading;
 
-  @override
-  void initState() {
-    super.initState();
+  // 选择宿舍楼
+  void _handlePowerPicker() async {
+    showPicker(context, _scaffoldKey,
+        title: "选择宿舍楼",
+        pickerDatas: PowerProvider.apartment,
+        colorRight: themeProvider.colorMain,
+        isArray: false, onConfirm: (Picker picker, List value) {
+      String home = picker.getSelectedValues()[0].toString();
+      powerProvider.powerBuilding = home;
+    });
+  }
+
+  // 跳转充值页面
+  void _charge() {
+    FlyDialogDIYShow(context, content: Wrap(
+      runSpacing: spaceCardPaddingTB,
+      children: [
+        FlyText.title45("请在充值页面点击图示按钮，前往充值",maxLine: 10,),
+        Image.asset("images/powerRechargeHelp.png"),
+        _buildButton("知道啦，前往充值页面↗",onTap: (){
+          launchUrl(
+              Uri.parse("http://ykt.cumt.edu.cn:8988/web/common/checkEle.html"));
+        }),
+      ],
+    ));
+
   }
 
   @override
@@ -55,65 +80,63 @@ class _PowerPageState extends State<PowerPage> {
     powerLoading = context.select((PowerProvider p) => p.powerLoading);
     return Scaffold(
       key: _scaffoldKey,
-      appBar: FlyAppBar(
-        context,
-        "宿舍电量",
-        actions: [
-          IconButton(onPressed: (){
-            launchUrl(Uri.parse("http://ykt.cumt.edu.cn:8988/web/common/checkEle.html"));
-            }, icon: Icon(MdiIcons.batteryPositive,color: Theme.of(context).primaryColor))
-        ]
-      ),
-      body: Container(
-        height: double.infinity,
-        width: double.infinity,
-        padding: EdgeInsets.fromLTRB(spaceCardMarginRL, spaceCardMarginTB,
-            spaceCardMarginRL, spaceCardMarginTB),
-        child: Column(
-          children: [
-            PowerCircularView(
-                powerPercent: powerPercent, themeProvider: themeProvider),
-            SizedBox(
-              height: spaceCardPaddingTB * 2,
-            ),
-            FlyText.title50(
-              powerPerviewText,
-              color: themeProvider.colorMain,
-              fontWeight: FontWeight.bold,
-            ),
-            SizedBox(
-              height: spaceCardPaddingTB * 3,
-            ),
-            _container(child: _buildPower(powerBuilding)),
-          ],
+      appBar: FlyAppBar(context, "宿舍电量"),
+      body: SingleChildScrollView(
+        physics: BouncingScrollPhysics(),
+        child: Container(
+          width: double.infinity,
+          padding: EdgeInsets.fromLTRB(spaceCardMarginRL, spaceCardMarginTB,
+              spaceCardMarginRL, spaceCardMarginTB),
+          child: Column(
+            children: [
+              PowerCircularView(
+                  powerPercent: powerPercent, themeProvider: themeProvider),
+              SizedBox(
+                height: spaceCardPaddingTB * 2,
+              ),
+              FlyText.title50(
+                powerPerviewText,
+                color: themeProvider.colorMain,
+                fontWeight: FontWeight.bold,
+              ),
+              SizedBox(
+                height: spaceCardPaddingTB * 3,
+              ),
+              _buildPower(powerBuilding),
+              SizedBox(
+                height: spaceCardPaddingTB * 3,
+              ),
+              _container(
+                  title: "充值",
+                  child: Wrap(
+                    runSpacing: spaceCardPaddingTB,
+                    children: [
+                      _buildButton("前往充值", primer:false,onTap: () => _charge()),
+                    ],
+                  )),
+            ],
+          ),
         ),
       ),
     );
   }
 
-  void _handlePowerPicker() async {
-    showPicker(context, _scaffoldKey,
-        title: "选择宿舍楼",
-        pickerDatas: PowerProvider.apartment,
-        colorRight: themeProvider.colorMain,
-        isArray: false, onConfirm: (Picker picker, List value) {
-          String home = picker.getSelectedValues()[0].toString();
-          powerProvider.powerBuilding = home;
-        });
-  }
-
-  InkWell _buildButton(String title, {GestureTapCallback onTap}) {
+  InkWell _buildButton(String title, {bool primer = true,GestureTapCallback onTap}) {
+    Color borderColor = primer?Colors.transparent:themeProvider.colorMain;
+    Color textColor = primer?Colors.white:themeProvider.colorMain;
+    Color backgroundColor = primer?themeProvider.colorMain:Colors.transparent;
     return InkWell(
       onTap: onTap,
       child: Container(
         padding: EdgeInsets.all(spaceCardMarginRL),
         decoration: BoxDecoration(
             borderRadius: BorderRadius.circular(borderRadiusValue),
-            color: themeProvider.colorMain),
+            border: Border.all(color: borderColor),
+            color: backgroundColor),
         child: Center(
           child: FlyText.title45(
             title,
-            color: Colors.white,
+            color: textColor,
             fontWeight: FontWeight.bold,
           ),
         ),
@@ -175,7 +198,7 @@ class _PowerPageState extends State<PowerPage> {
         child: _buildInputBar("输入寝室号(如M2B421、Z1B104)", _powerRoomidController));
   }
 
-  Widget _container({@required Widget child}) {
+  Widget _container({@required String title, @required Widget child}) {
     return Container(
       decoration: BoxDecoration(
           borderRadius: BorderRadius.circular(borderRadiusValue),
@@ -183,7 +206,19 @@ class _PowerPageState extends State<PowerPage> {
       width: double.infinity,
       padding: EdgeInsets.fromLTRB(spaceCardPaddingRL, spaceCardPaddingTB / 2,
           spaceCardPaddingRL, spaceCardPaddingTB * 2),
-      child: child,
+      child: Wrap(
+        runSpacing: spaceCardPaddingTB,
+        children: [
+          Container(),
+          Row(
+            children: [FlyText.mainTip40(title)],
+          ),
+          Divider(
+            height: 0,
+          ),
+          child
+        ],
+      ),
     );
   }
 
@@ -209,24 +244,27 @@ class _PowerPageState extends State<PowerPage> {
   Widget _buildPower(String powerBuilding) {
     return Padding(
       padding: EdgeInsets.fromLTRB(spaceCardMarginRL, 0, spaceCardMarginRL, 0),
-      child: Wrap(runSpacing: spaceCardPaddingTB, children: [
-        _buildPreviewButton("宿舍楼", powerBuilding ?? "未选择",
-            onTap: () => _handlePowerPicker()),
-        _buildInputButton("宿舍号"),
-        FlyWidgetBuilder(
-            whenFirst: powerLoading,
-            firstChild: _buildButton(
-              "加载中……",
-              onTap: () {},
-            ),
-            secondChild: _buildButton(
-              "绑定",
-              onTap: () {
-                powerProvider.powerRoomid = _powerRoomidController.text ?? "";
-                powerProvider.bindInfoAndGetPower(context);
-              },
-            )),
-      ]),
+      child: _container(
+          title: "绑定信息",
+          child: Wrap(runSpacing: spaceCardPaddingTB, children: [
+            _buildPreviewButton("宿舍楼", powerBuilding ?? "未选择",
+                onTap: () => _handlePowerPicker()),
+            _buildInputButton("宿舍号"),
+            FlyWidgetBuilder(
+                whenFirst: powerLoading,
+                firstChild: _buildButton(
+                  "加载中……",
+                  onTap: () {},
+                ),
+                secondChild: _buildButton(
+                  "绑定",
+                  onTap: () {
+                    powerProvider.powerRoomid =
+                        _powerRoomidController.text ?? "";
+                    powerProvider.bindInfoAndGetPower(context);
+                  },
+                )),
+          ])),
     );
   }
 }
