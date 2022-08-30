def943f02a73ac4acae01cc741f6997baf63233c
CUMT-Atom <cnatom@foxmail.com>
2022-08-30 14:36:35 +0800

    ♻️refactor(navigator_page):提高可读性

--- lib/pages/navigator_page.dart
+++ lib/pages/navigator_page.dart
@@ -16,125 +16,156 @@ import 'package:outline_material_icons/outline_material_icons.dart';
 import 'package:provider/provider.dart';
 import 'app_upgrade.dart';
 import 'navigator_page_child/course_table/course_page.dart';
+import 'package:flutter_intro/flutter_intro.dart';
 
-Future<void> sendInfo(String page,String action)async{
+Future<void> sendInfo(String page, String action) async {
   var info = {
-    "username":Prefs.username,
-    "action":action,
-    "page":page,
-    "info":{
-      "name":Prefs.name,
-      "time":DateTime.now().toString(),
-      "system":Platform.operatingSystem,
-      "version":Global.curVersion
+    "username": Prefs.username,
+    "action": action,
+    "page": page,
+    "info": {
+      "name": Prefs.name,
+      "time": DateTime.now().toString(),
+      "system": Platform.operatingSystem,
+      "version": Global.curVersion
     }
   };
-  Dio().post(
-    "https://user.kxz.atcumt.com/admin/action",
-    data: info
-  );
-  print("sendInfo:"+page+':'+action);
+  Dio().post("https://user.kxz.atcumt.com/admin/action", data: info);
+  print("sendInfo:" + page + ':' + action);
 }
-//跳转到当前页面
-void toNavigatorPage(BuildContext context){
-  Navigator.of(context).pushAndRemoveUntil(CustomRoute(FlyNavigatorPage(),milliseconds: 500),(route)=>route==null);
-  sendInfo('App', '打开');
 
+//跳转到当前页面
+void toNavigatorPage(BuildContext context) {
+  Navigator.of(context).pushAndRemoveUntil(
+      CustomRoute(FlyNavigatorPage(), milliseconds: 500),
+      (route) => route == null);
+  sendInfo('App', '打开');
 }
+
+class BottomNavigationBarModel {
+  final String title;
+  final IconData iconData;
+
+  BottomNavigationBarModel(this.title, this.iconData);
+}
+
 //底部导航栏页面,位于子页面的顶端
 class FlyNavigatorPage extends StatefulWidget {
   FlyNavigatorPageState createState() => FlyNavigatorPageState();
 }
+
 var navigatorPageController = PageController(initialPage: 0);
-class FlyNavigatorPageState extends State<FlyNavigatorPage> with AutomaticKeepAliveClientMixin,WidgetsBindingObserver {
+
+class FlyNavigatorPageState extends State<FlyNavigatorPage>
+    with AutomaticKeepAliveClientMixin, WidgetsBindingObserver {
   int _currentIndex = 0; //数组索引，通过改变索引值改变视图
-  static GlobalKey<NavigatorState> navigatorKey=GlobalKey();
+  static GlobalKey<NavigatorState> navigatorKey = GlobalKey();
   ThemeProvider themeProvider;
+
   //校园网自动登录
-  void cumtAutoLogin()async{
-    if(Prefs.cumtLoginUsername!=null){
+  void cumtAutoLogin() async {
+    if (Prefs.cumtLoginUsername != null) {
       await cumtAutoLoginGet(context,
           username: Prefs.cumtLoginUsername,
           password: Prefs.cumtLoginPassword,
           loginMethod: Prefs.cumtLoginMethod);
     }
   }
+
   @override
   void initState() {
     super.initState();
-    cumtAutoLogin();//自动登录校园网
+    cumtAutoLogin(); //自动登录校园网
     WidgetsBinding.instance.addObserver(this);
-    checkUpgrade(context);//检查软件更新
-
-
+    checkUpgrade(context); //检查软件更新
   }
+
   @override
   void dispose() {
     WidgetsBinding.instance.removeObserver(this);
     super.dispose();
   }
+
   @override
   void didChangeAppLifecycleState(AppLifecycleState state) {
-    if(state == AppLifecycleState.resumed){
+    if (state == AppLifecycleState.resumed) {
       cumtAutoLogin();
     }
   }
-  BottomNavigationBarItem _bottomNavigationBar(String title,IconData iconData,{double size})=>BottomNavigationBarItem(
-      label: title,
-      icon: Icon(
-        iconData,
-        size: size,
-      )
-  );
+
+  final List<BottomNavigationBarModel> _buttonBarItem = [
+    BottomNavigationBarModel("主页", FeatherIcons.home),
+    BottomNavigationBarModel(
+      '发现',
+      OMIcons.explore,
+    ),
+    BottomNavigationBarModel('我的', Icons.person_outline),
+  ];
+
+  BottomNavigationBarItem _bottomNavigationBar(String title, IconData iconData,
+          {double size}) =>
+      BottomNavigationBarItem(
+          label: title,
+          icon: Icon(
+            iconData,
+            size: size,
+          ));
+
   @override
   Widget build(BuildContext context) {
     super.build(context);
     themeProvider = Provider.of<ThemeProvider>(context);
     return FlyNavBackground(
-        child: Scaffold(
-          backgroundColor: Colors.transparent,
-          body: PageView(
-            physics: NeverScrollableScrollPhysics(),
-            children: [
-              CoursePage(),
-              // InfoPage(),
-              DiyPage(),
-              MyselfPage(),
-              // TestPage()
-            ],
-            controller: navigatorPageController,
-            onPageChanged: (int index){
-              setState(() {
-                if(_currentIndex!=index) _currentIndex = index;
-              });
-            },
-          ),
-          bottomNavigationBar: BottomNavigationBar(
-              elevation: 0,
+        child: Intro(
+            padding: const EdgeInsets.all(8),
+            borderRadius: BorderRadius.all(Radius.circular(4)),
+            maskColor: const Color.fromRGBO(0, 0, 0, .6),
+            noAnimation: false,
+            maskClosable: false,
+            buttonTextBuilder: (order) =>
+                order == 3 ? 'Custom Button Text' : 'Next',
+            child: Scaffold(
               backgroundColor: Colors.transparent,
-              selectedFontSize: fontSizeTip33,
-              unselectedFontSize: fontSizeTip33,
-              selectedItemColor: themeProvider.simpleMode?themeProvider.colorMain:null,
-              unselectedItemColor: themeProvider.simpleMode?Colors.black45:null,
-              items: [
-                _bottomNavigationBar('主页',FeatherIcons.home,),
-                // _bottomNavigationBar("资讯", Icons.article_outlined,badgeShowList[1]),
-                _bottomNavigationBar('发现',OMIcons.explore,),
-                _bottomNavigationBar('我的',Icons.person_outline,),
-                // _bottomNavigationBar('我的',Icons.person_outline,badgeShowList[3]),
+              body: _buildBody(),
+              bottomNavigationBar: _buildBottomNavigationBar(),
+            )));
+  }
 
-              ],
-              currentIndex: _currentIndex,
-              onTap: (int index) {
-                navigatorPageController.jumpToPage(index);
-              },
-              type: BottomNavigationBarType.fixed),
-        )
+  BottomNavigationBar _buildBottomNavigationBar() {
+    return BottomNavigationBar(
+        items: _buttonBarItem
+            .map((item) => _bottomNavigationBar(item.title, item.iconData)),
+        onTap: (int index) {
+          navigatorPageController.jumpToPage(index);
+        },
+        elevation: 0,
+        backgroundColor: Colors.transparent,
+        selectedFontSize: fontSizeTip33,
+        unselectedFontSize: fontSizeTip33,
+        selectedItemColor:
+            themeProvider.simpleMode ? themeProvider.colorMain : null,
+        unselectedItemColor: themeProvider.simpleMode ? Colors.black45 : null,
+        currentIndex: _currentIndex,
+        type: BottomNavigationBarType.fixed);
+  }
+
+  PageView _buildBody() {
+    return PageView(
+      physics: NeverScrollableScrollPhysics(),
+      children: [
+        CoursePage(),
+        DiyPage(),
+        MyselfPage(),
+      ],
+      controller: navigatorPageController,
+      onPageChanged: (int index) {
+        setState(() {
+          if (_currentIndex != index) _currentIndex = index;
+        });
+      },
     );
   }
+
   @override
   bool get wantKeepAlive => true;
 }
-
-
-
